96171d4c4b272a59938ba2a50450db6eac11bda4
SoeJimenezLuna <119030140+SoeJimenezLuna@users.noreply.github.com>
2023-05-30 11:13:02 -0600

    codigo nuevo

---
+++ laravel-app/database/migrations/2023_05_30_154435_add_avatar_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()//modificar una tabla que ya existe, el nombre de la tabla, una funcion anonima y recibe un objeto
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //modificaciones
+            $table->string('avatar')->nullable()->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+        });
+    }
+};
